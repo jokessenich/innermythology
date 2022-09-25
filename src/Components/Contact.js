da518683d0ea60fa04a044799906242639839f97
jokessenich <johnny.kessenich@gmail.com>
2022-09-25 16:22:04 -0400

    added calendly

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -21,9 +21,14 @@ render(){
 
         <h1 className="page-header">Sessions</h1>
         <p className="description-section">
-            I offer a first virtual session for free to see if Inner Mythology is a good fit for you.
+            I offer a 30 minute virtual session for free to see if Inner Mythology is a good fit for you.{<br />}{<br />}
+            <a className="scheduleLink" href="https://calendly.com/innermythology/intro-session">Click here to schedule a free introductory session.</a>
+            {<br />}{<br />}
+            {<br />}{<br />}
+
             Virtual: An inner exploration with me via zoom. The session will be between 1 and 2 hours.
 {<br />}{<br />}
+
 
 In Person: I offer three ways of connecting in person. 
 <li className = "session-types">An in person inner exploration.</li>
@@ -41,14 +46,17 @@ In Person: I offer three ways of connecting in person.
                 <li>$80 for Inner Exploration in zoom or in person</li>
                 <li>$150 for Inner Exploration with Reiki or Massage</li>
             </ul>
-            {<br />}{<br />}
+
             We do offer some sliding scale servicess, contact for more information.
-                    
+            {<br />}{<br />}
+
+            <a className="scheduleLink" href="https://calendly.com/innermythology/full-session">Click here to schedule a full length session.</a>
         </p>
         <div className="contact-section">
         {this.state.hidden?<h3 onClick = {()=>this.setState({hidden:false})}>Click here for contact information</h3>:""}
         {this.state.hidden?'':<div id = "contactInfo">
             <h3 className="emailLink">For sessions or questions: </h3><br /><br /><a className="emailLink bigger" href="mailto:innermythology@gmail.com">innermythology (at) gmail.com </a>
+            {<br />}{<br />}
             </div>}
             </div>
         </div>
